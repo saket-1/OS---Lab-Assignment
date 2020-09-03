a9b3a1223bf1ffaa4916113b969ea4e2a5d2d079
saket-1 <65917705+saket-1@users.noreply.github.com>
2020-09-03 15:07:45 +0530

    Update Q5(b).c

--- Q5(b).c
+++ Q5(b).c
@@ -2,30 +2,23 @@
 #include <stdlib.h> 
 #include <unistd.h> 
 #include <pthread.h> 
-  	
-
-int a_global = 0; 
-  
-void *myThread(void *x) 
+int g = 0; //global variable
+void *Function(void *vargp) 
 { 
-    
-    int *myid = (int *)x; 
-    static int a_static = 0; 
-    int a_local =0;
-  
-    printf("Thread ID: %d, Static: %d, Global: %d, Local: %d\n", *myid, ++a_static, ++a_global, ++a_local); 
+    int *myid = (int *)vargp; 
+    static int s = 0; //static variable
+    ++s; 
+    ++g; 
+    int l= 9;//local variable
+    printf("Thread ID: %d, Static: %d, Global: %d, Local: %d\n", *myid,s,g,l); 
 } 
-  
 int main() 
 { 
-     
-    pthread_t tid1; 
-    pthread_t tid2; 
-    pthread_t tid3; 
-    
-    pthread_create(&tid1, NULL, myThread, (void *)&tid1);
-    pthread_create(&tid2, NULL, myThread, (void *)&tid2);
-    pthread_create(&tid3, NULL, myThread, (void *)&tid3);
+    int i; 
+    pthread_t tid; 
+    //Creation of three threads 
+    for (i = 0; i < 3; i++) 
+        pthread_create(&tid, NULL, Function, (void *)&tid); 
   
     pthread_exit(NULL); 
     return 0; 
